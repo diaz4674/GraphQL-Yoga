ee53d0b0127e1f2e645442f633b6c56ae13b83b9
miguel diaz <diaz4674@gmail.com>
2020-09-14 21:22:38 -0700

    added nodemon

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,30 @@
-console.log("hi");
+import { GraphQLServer } from "graphql-yoga";
+
+// Type definitions
+const typeDefs = `
+    type Query {
+        hello: String!
+        name: String
+    }
+`;
+
+// Resolvers
+const resolvers = {
+	Query: {
+		hello() {
+			return "This is my first query!";
+		},
+		name() {
+			return "Migz";
+		},
+	},
+};
+
+const server = new GraphQLServer({
+	typeDefs,
+	resolvers,
+});
+
+server.start(() => {
+	console.log("The server is up!");
+});
