b8f9e8fb3c2923bb2b2dab1a72d77de5e61ed486
miguel diaz <diaz4674@gmail.com>
2020-09-18 22:26:52 -0700

    added relationship

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,12 +1,58 @@
 import { GraphQLServer } from "graphql-yoga";
 
+// Demo user data
+const users = [
+	{
+		id: "1",
+		name: "Miguel",
+		email: "miguel@example.com",
+		age: 27,
+	},
+	{
+		id: "2",
+		name: "Mike",
+		email: "mil@example.com",
+		age: 27,
+	},
+	{
+		id: "3",
+		name: "Sarah",
+		email: "sara@example.com",
+		age: 27,
+	},
+];
+
+const posts = [
+	{
+		id: "1",
+		title: "Turtle Ninja",
+		body: "There were turtles",
+		published: true,
+		author: "1",
+	},
+	{
+		id: "2",
+		title: "The Notebooke",
+		body: "A guy and a girl. he built her a home",
+		published: true,
+		author: "2",
+	},
+	{
+		id: "3",
+		title: "Texas Chainsaw Massacre",
+		body: "Some dude had a weird fetish",
+		published: true,
+		author: "3",
+	},
+];
+
 // Type definitions
 const typeDefs = `
     type Query {
-		greeting(name: String, position: String): String!
+		users(query: String): [User!]!
 		me: User!
-		add(num1: Float, num2: Float): Float!
 		post: Post!
+		posts(query: String): [Post!]
 	}
 
 	type User {
@@ -21,18 +67,21 @@ const typeDefs = `
 		title: String!
 		body: String!
 		published: Boolean!
+		author: User!
 	}
 `;
 
 // Resolvers
 const resolvers = {
 	Query: {
-		greeting(parent, args, ctx, info) {
-			if (args.name && args.position) {
-				return `Hello ${args.name}! You are my favorite ${args.position}`;
-			} else {
-				return "Hello user!";
+		users(parent, args, ctx, info) {
+			if (!args.query) {
+				return users;
 			}
+
+			return users.filter((user) => {
+				return user.name.toLowerCase().includes(args.query.toLowerCase());
+			});
 		},
 		me() {
 			return {
@@ -42,9 +91,6 @@ const resolvers = {
 				age: 28,
 			};
 		},
-		add(parent, args, ctx, info) {
-			return args.num1 + args.num2;
-		},
 		post() {
 			return {
 				id: "092",
@@ -52,6 +98,27 @@ const resolvers = {
 				body: "",
 				published: false,
 			};
+		},
+		posts(parent, args, ctx, info) {
+			if (!args.query) {
+				return posts;
+			}
+
+			return posts.filter((post) => {
+				if (
+					post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+					post.body.toLowerCase().includes(args.query.toLowerCase())
+				) {
+					return post;
+				}
+			});
+		},
+	},
+	Post: {
+		author(parent, args, ctx, info) {
+			return users.find((user) => {
+				return user.id === parent.author;
+			});
 		},
 	},
 };
