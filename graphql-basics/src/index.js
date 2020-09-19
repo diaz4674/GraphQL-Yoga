5a22abebe68bf3aab1915c2a36c2c306c288c5dd
miguel diaz <diaz4674@gmail.com>
2020-09-18 21:48:29 -0700

    added args

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -3,7 +3,10 @@ import { GraphQLServer } from "graphql-yoga";
 // Type definitions
 const typeDefs = `
     type Query {
+		greeting(name: String, position: String): String!
 		me: User!
+		add(num1: Float, num2: Float): Float!
+		post: Post!
 	}
 
 	type User {
@@ -13,17 +16,41 @@ const typeDefs = `
 		age: Int
 	}
 	
+	type Post {
+		id: ID!,
+		title: String!
+		body: String!
+		published: Boolean!
+	}
 `;
 
 // Resolvers
 const resolvers = {
 	Query: {
+		greeting(parent, args, ctx, info) {
+			if (args.name && args.position) {
+				return `Hello ${args.name}! You are my favorite ${args.position}`;
+			} else {
+				return "Hello user!";
+			}
+		},
 		me() {
 			return {
 				id: "123456",
 				name: "Mike",
 				email: "mike@example.com",
 				age: 28,
+			};
+		},
+		add(parent, args, ctx, info) {
+			return args.num1 + args.num2;
+		},
+		post() {
+			return {
+				id: "092",
+				title: "GraphQL",
+				body: "",
+				published: false,
 			};
 		},
 	},
