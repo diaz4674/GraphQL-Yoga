39ceab3d8a6ab858431c3cca82c0022851979ad1
miguel diaz <diaz4674@gmail.com>
2020-09-22 20:14:46 -0700

    added update method

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -47,6 +47,36 @@ const Mutation = {
 
 		return deletedUsers[0];
 	},
+	updateUser(parent, args, { db }, info) {
+		const { id, data } = args;
+		const user = db.users.find((user) => {
+			return user.id === id;
+		});
+
+		if (!user) {
+			throw new Error("user not found");
+		}
+
+		if (typeof data.email === "string") {
+			const emailTaken = db.users.some((user) => user.email === data.email);
+
+			if (emailTaken) {
+				throw new Error("email already taken");
+			}
+
+			user.email = data.email;
+		}
+
+		if (typeof data.name === "string") {
+			user.name = data.name;
+		}
+
+		if (typeof data.age !== "undefined") {
+			user.age = data.age;
+		}
+
+		return user;
+	},
 	createPost(parent, args, { db }, info) {
 		const userExists = db.users.some((user) => user.id === args.data.author);
 
